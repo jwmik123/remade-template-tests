5595c81a1b46627305fd459cc711456d6590f397
Joël Mik <contact@joelmik.nl>
2023-02-20 21:54:07 +0100

    too heavy

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+export const About = () => {
+  return (
+    <section>
+      <p>
+        When you first started your company, you wanted to save money wherever
+        you could. So, you decided to take on the task of building your own
+        website and branding You poured hours of your time into it, and even
+        though it wasn’t perfect, it served its purpose at the time. But as your
+        company has grown or needs to grow, you’ve realized that You’re
+        receiving fewer and fewer leads, and it’s becoming harder to convert
+        visitors into customers. You’ve also come to realize is no longer
+        cutting it in today’s competitive market. It’s time to invest in a
+        professional website and branding that truly represents your brand and
+        converts visitors into paying customers.
+      </p>
+    </section>
+  );
+};
+
+export default About;
